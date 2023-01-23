e71518e6253190b0b13766c8e30a1dbdbcd2da01
kath <katharineturner3@gmail.com>
2023-01-23 16:55:32 +0000

    feat(small)-update-nav-links-and-styling

--- src/components/shared/Nav.tsx
+++ src/components/shared/Nav.tsx
@@ -1,5 +1,5 @@
-// Styles
-import "./Nav.css"
+// Libs
+import { Link } from "react-router-dom";
 
 // Types
 type NavType = {
@@ -8,11 +8,16 @@ type NavType = {
 }
 
 export const Nav = ({containerClass, itemsClass}: NavType) => {
+    const linkStyles = {
+        textDecoration: "none",
+        color: "inherit"
+    }
+    
     return (
       <nav className={containerClass}>
-        <li className={`link ${itemsClass}`}>about</li>
-        <li className={`link ${itemsClass}`}>portfolio</li>
-        <li className={`link ${itemsClass}`}>contact</li>
+        <Link style={linkStyles} to={`/about`}><p className={`link ${itemsClass}`}>about</p></Link>
+        <Link  style={linkStyles} to={`/portfolio`}><p className={`link ${itemsClass}`}>portfolio</p></Link>
+        <Link  style={linkStyles} to={`/contact`}><p className={`link ${itemsClass}`}>contact</p></Link>
       </nav>
     );
 }
