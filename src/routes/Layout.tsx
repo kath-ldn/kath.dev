1ad193993d1c90bc25025b1d625dbf5ff2599a48
kath <katharineturner3@gmail.com>
2023-01-24 17:24:48 +0000

    feat(small-add-scroll-to-top-on-navigation

--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -1,6 +1,6 @@
 // Libs
-import { useContext } from "react";
-import { Outlet } from "react-router-dom";
+import { useContext, useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 // Components
 import { HamburgerMenu } from "../components/layout/HamburgerMenu";
 import Mode from "../components/layout/Mode";
@@ -11,6 +11,16 @@ import { ModeContext } from "../contexts/ModeContext";
 
 export default function Layout() {
     const { mode, setMode } = useContext(ModeContext)
+    const { pathname } = useLocation();
+
+    // Scroll to top when route changes - this happens here as this is the entry element for BrowserRouter
+    useEffect(() => {
+      document.documentElement.scrollTo({
+        top: 0,
+        left: 0,
+      });
+    }, [pathname]);
+
     return (
       <div className={`${mode === "light" ? "mode-light" : "mode-dark"} flex-column align-center`}>
         {/* hamburger menu (mob only) */}
